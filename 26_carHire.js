ac910aec4c124079fab8ddd0d41e9c6c9a79d61b
thehicksivist <hicks.a.james@gmail.com>
2019-01-08 18:06:32 +1000

    26 completed

--- 26_carHire.js
+++ 26_carHire.js
@@ -23,6 +23,17 @@
 
 const rentalCarCost = (d) => {
     // Your code here!
+    let cost = d * 40
+//     Every day that you rent the car costs $40.
+    if (d >= 7) {
+        cost -= 50
+        return cost
+    } else if (d >= 3) {
+        cost -= 20
+        return cost
+    }
+
+    return cost
 }
 
 
